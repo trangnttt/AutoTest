090fc455bfac6c461171e72c1ef34f1bcb7080a1
trangnttt <ngotrang447@gmai.com>
2025-04-11 21:39:51 +0700

    update 110425

--- Playwright/saucedemo/tests/add_to_cart.spec.ts
+++ Playwright/saucedemo/tests/add_to_cart.spec.ts
@@ -4,9 +4,9 @@ import { login } from "../utils/helper";
 test('add to cart 1 product success', async ({ page }) => {
     await login(page);
     await page.click('#add-to-cart-sauce-labs-backpack');
-    expect(page.locator('.shopping_cart_badge')).toHaveText('1');
+    await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
     await page.click('#shopping_cart_container');
-    expect(page.locator('.inventory_item_name')).toHaveText('Sauce Labs Backpack');
+    await expect(page.locator('.inventory_item_name')).toHaveText('Sauce Labs Backpack');
 
 })
 test('add to cart 2 product success', async ({ page }) => {
@@ -15,5 +15,22 @@ test('add to cart 2 product success', async ({ page }) => {
     await page.click('#add-to-cart-sauce-labs-bike-light');
     expect(page.locator('.shopping_cart_badge')).toHaveText('2');
     await page.click('#shopping_cart_container');
-    expect(page.locator('.cart_item')).toHaveCount(2);
+    await expect(page.locator('.cart_item')).toHaveCount(2);
+})
+
+// test.only('add to cart multiple products', async ({ page }) => {
+//     await login(page);
+//     const addToCartButtons = page.locator('.btn_inventory');
+//     const numberOfProductsToAdd = 2;
+//     for (let i = 0; i < numberOfProductsToAdd; i++) {
+//         await addToCartButtons.nth(i).click();
+//     }
+//     await expect(page.locator('.shopping_cart_badge')).toHaveText(String(numberOfProductsToAdd));
+// })
+test.only('add and remove product', async ({ page }) => {
+    await login(page);
+    await page.click('#add-to-cart-sauce-labs-backpack');
+    await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
+    await page.click('#remove-sauce-labs-backpack')
+    await expect(page.locator('.shopping_cart_badge')).toHaveCount(0);
 })
